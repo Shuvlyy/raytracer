b3964475e943a6470385f4b02ab3c00ca9771404
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-04-23 19:22:06 +0200

    feat(build): Now uses Logger library.

--- mk/flags.mk
+++ mk/flags.mk
@@ -8,6 +8,8 @@
 CXXFLAGS	+= -Wall -Wextra -Werror
 CXXFLAGS	+= -std=c++20
 CXXFLAGS	+= -iquote src/
+CXXFLAGS	+= -iquote lib/Logger/include/
 CXXFLAGS	+= -g # Only for debug.
 
 LDFLAGS		+= -lsfml-window -lsfml-graphics -lsfml-system
+LDFLAGS		+= -L lib/.output -l logger
