666d1d2aa179a2e30af9fcbe813cecacc23c60c4
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-04-26 20:07:50 +0200

    feat(build): Now includes YML parser and Logger.

--- mk/flags.mk
+++ mk/flags.mk
@@ -7,9 +7,12 @@
 
 CXXFLAGS	+= -Wall -Wextra -Werror
 CXXFLAGS	+= -std=c++20
-CXXFLAGS	+= -iquote src/
+CXXFLAGS	+= -iquote src/ # include/
 CXXFLAGS	+= -iquote lib/Logger/include/
+CXXFLAGS	+= -iquote lib/YmlParser/src/
 CXXFLAGS	+= -g # Only for debug.
 
+LDFLAGS		+= -L lib/.output/
+LDFLAGS		+= -l logger
+LDFLAGS		+= -l yml
 LDFLAGS		+= -lsfml-window -lsfml-graphics -lsfml-system
-LDFLAGS		+= -L lib/.output -l logger
