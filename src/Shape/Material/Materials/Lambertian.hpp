737aebb6d42eb8f99e675b3200672d5c89538704
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-04-28 01:13:09 +0200

    feat(shape>material): Added Lambertian-reflection material.

---
+++ src/Shape/Material/Materials/Lambertian.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Shape/Material/Material.hpp"
+#include "Shape/Shape.hpp"
+
+namespace raytracer::shape::material
+{
+
+    class Lambertian final
+        : public Material
+    {
+    public:
+        explicit Lambertian(const math::Color& color) : _color(color) {}
+
+        [[nodiscard]] bool scatter(
+            const math::Ray&,
+            HitResult& res,
+            math::Color& attenuation,
+            math::Ray& scattered
+        ) const override
+        {
+            auto dir = res.n + math::Vec<3>::random();
+
+            scattered = math::Ray(res.p, dir);
+            attenuation = this->_color;
+            return true;
+        }
+
+    private:
+        math::Color _color;
+    };
+
+}
