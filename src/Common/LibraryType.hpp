67f6059e812a6f726bbe5818285c50994dc8f22f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-04-30 19:12:19 +0200

    feat(library): Added LibraryType enum class.

---
+++ src/Common/LibraryType.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace raytracer::library
+{
+
+    enum class Type
+    {
+        IMAGE       = 0x220405,
+        SHAPE       = 0x130608,
+        LIGHT       = 0x180125,
+        MATERIAL    = 0x220325
+    };
+
+}
