2c5df9e8c5c316bad66ff7109ecc2ebf538a9a2f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-04-30 15:47:57 +0200

    feat(light): Added `Directional` light source class.

---
+++ src/Light/Lights/Directional.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Light/Light.hpp"
+
+namespace raytracer::light
+{
+
+    /**
+     * Directional lights represent sources like the sun, where all light rays
+     * are parallel, and the light has no specific position. Intensity does not
+     * diminish with distance.
+     */
+    class Directional final
+        : public Light
+    {
+    public:
+        /**
+         * @brief   Constructs a directional light.
+         *
+         * @param   color       The color/intensity of the light
+         * @param   direction   The direction from which the light is coming
+         *                      (will be negated in shading)
+         */
+        Directional
+        (
+            const math::Color& color,
+            const math::Vec<3>& direction
+        ) : _color(color), _direction(direction) {}
+
+        [[nodiscard]] math::Color sample
+        (
+            const math::Vec<3>& point,
+            math::Vec<3>& directionToNormal,
+            double& distance
+        )
+            const override
+        {
+            directionToNormal = -this->_direction;
+            distance = std::numeric_limits<double>::infinity();
+            return this->_color;
+        }
+
+    private:
+        math::Color _color;
+        math::Vec<3> _direction;
+    };
+
+}
