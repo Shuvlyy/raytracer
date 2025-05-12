efda737d4882e2af98aee6f2d08eb9e84efb73e9
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-05-12 09:13:08 +0200

    feat(network>packet): Added `PING` and `PONG` packet.

---
+++ src/Network/Packet/Packets/Pong.cpp
@@ -0,0 +1,50 @@
+#include "Pong.hpp"
+
+#include "Exception/Exceptions/UnexpectedRemainingData.hpp"
+
+#include "../IO/Deserializer.hpp"
+#include "../IO/Serializer.hpp"
+
+namespace raytracer::server::packet
+{
+
+    Pong::Pong
+    (
+        const uint64_t timestamp
+    )
+        : Packet(Type::PONG),
+          _timestamp(timestamp)
+    {}
+
+    ByteBuffer
+    Pong::serialize
+    ()
+        const
+    {
+        Serializer s;
+
+        s.write<uint8_t>(static_cast<uint8_t>(this->_type));
+        s.write<uint64_t>(this->_timestamp);
+        return s.data();
+    }
+
+    void
+    Pong::deserialize
+    (
+        const uint8_t *data,
+        const size_t size
+    )
+    {
+        Deserializer d({ data, data + size });
+
+        const auto rawType = d.read<uint8_t>();
+        const Type type = fromRawTypeToType(rawType);
+
+        this->_timestamp = d.read<uint64_t>();
+
+        if (d.hasRemaining()) {
+            throw exception::UnexpectedRemainingData(type);
+        }
+    }
+
+}
---
+++ src/Network/Packet/Packets/Pong.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../Packet.hpp"
+
+namespace raytracer::server::packet
+{
+
+    class Pong final
+        : public Packet
+    {
+    public:
+        explicit Pong(uint64_t timestamp = 0);
+
+        [[nodiscard]] ByteBuffer serialize() const override;
+        void deserialize(const uint8_t *data, size_t size) override;
+
+        [[nodiscard]] uint64_t getTimestamp() const { return this->_timestamp; }
+
+    private:
+        uint64_t _timestamp;
+    };
+
+}
