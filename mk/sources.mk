6a1c62a7b1f0f2d1c8eaef028e578d6484903547
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-05-12 09:18:37 +0200

    feat(build): Added missing sources.

--- mk/sources.mk
+++ mk/sources.mk
@@ -27,3 +27,22 @@ SRC += src/Scene/Scene.cpp \
 	src/Factory/LightFactory.cpp \
 	src/Factory/GetColor.cpp \
 	src/Factory/GetVec3.cpp
+
+SRC += src/App.cpp
+
+SRC += src/Signal/Manager.cpp \
+	src/Signal/Handlers/Int.cpp \
+	src/Signal/Handlers/Term.cpp
+
+SRC += src/Network/Server/Server.cpp \
+	src/Network/Server/Socket/Socket.cpp \
+	src/Network/Server/Session/Manager.cpp \
+	src/Network/Server/Session/Session.cpp \
+	src/Network/Packet/Packet.cpp \
+	src/Network/Server/Packet/Manager.cpp \
+	src/Network/Packet/Packets/Ping.cpp \
+	src/Network/Packet/Packets/Pong.cpp \
+	src/Network/Packet/IO/Serializer.cpp \
+	src/Network/Packet/IO/Deserializer.cpp \
+	src/Network/Server/Packet/Handlers/Ping.cpp \
+	src/Network/Server/Packet/Handlers/Pong.cpp
