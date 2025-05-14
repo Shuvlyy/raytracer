83b21f4ee005f8a715ac793a27d5f3b4ae8b546f
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2025-05-14 11:37:31 +0200

    up(multithreading) | WIP

--- src/Multithreading/Multithreading.cpp
+++ src/Multithreading/Multithreading.cpp
@@ -13,32 +13,40 @@ namespace raytracer::multithreading
     render
     (
         Renderer &renderer,
-        int nbProcs
+        const int nbProcs
     )
     {
         int last_y = 0;
         int portion_size = std::floor(renderer.getHeight() / nbProcs);
-        LOG_INFO("Starting render with " + std::to_string(nbProcs) + " threads.");
         std::vector<std::thread> threads;
 
+        LOG_INFO("Starting render with " + std::to_string(nbProcs) + " threads.");
+
         if (nbProcs == 1) {
-            renderer.render(0, renderer.getHeight() - 1);
+            renderer.render(
+                0, 0,
+                renderer.getWidth(), renderer.getHeight() - 1
+            );
             return;
         }
+
         for (int i = 1; i < nbProcs + 1; i++) {
             if (i == nbProcs) {
-                threads.emplace_back(&Renderer::render, &renderer, last_y, renderer.getHeight() - 1);
+                // threads.emplace_back(&Renderer::render, &renderer, last_y, renderer.getHeight() - 1);
                 LOG_DEBUG(std::format("thread n°{} rendering from {} to {}.", i, last_y, renderer.getHeight() - 1));
             } else {
-                threads.emplace_back(&Renderer::render, &renderer, last_y, portion_size * i);
+                // threads.emplace_back(&Renderer::render, &renderer, last_y, portion_size * i);
                 LOG_DEBUG(std::format("thread n°{} rendering from {} to {}.", i, last_y, portion_size * i));
             }
             last_y = portion_size * i + 1;
         }
+
         // TODO: SFML display
-        for (auto & thread : threads) {
+
+        for (auto& thread : threads) {
             thread.join();
         }
+
         LOG_INFO("Render finished.");
     }
 
