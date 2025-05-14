8cf90edfd527b47e7b204f1bb80f36e6e825e296
Pierre Marguerie <pierre.marguerie@epitech.eu>
2025-05-14 18:00:02 +0200

    feat(network>client>app): Added separate thread to manage the network

--- src/App/Client/Client.cpp
+++ src/App/Client/Client.cpp
@@ -1,10 +1,9 @@
-#include "Exception/Exceptions/EmptyPacket.hpp"
-#include "Exception/Exceptions/ServerDisconnected.hpp"
-#include "Exception/Exceptions/StandardFunctionFail.hpp"
+#include "Network/Client/Packet/IHandler.hpp"
 
 #include "Client.hpp"
 
 #include <poll.h>
+#include <thread>
 
 namespace raytracer::app
 {
@@ -20,16 +19,23 @@ namespace raytracer::app
     void
     Client::run()
     {
-        this->_running = true;
         LOG_INFO("Client running...");
-        this->_client.run();
+        this->_running = true;
+        std::thread th(&network::Client::run, &this->_client);
+        while (this->_running) {
+            if (this->_client.hasPacketToProcess()) {
+                auto packet = this->_client.popPacket();
+                this->_manager.dispatchPacket(*packet, *this);
+            }
+        }
     }
 
     void
     Client::stop()
     {
-        this->_running = false;
         LOG_INFO("Client stopping...");
+        this->_running = false;
+        this->_client.stop();
     }
 
 }
--- src/App/Client/Client.hpp
+++ src/App/Client/Client.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <atomic>
+
+#include "Renderer.hpp"
 #include "App/App.hpp"
-#include "Network/Client/Channel/Channel.hpp"
 #include "Network/Client/Client.hpp"
+#include "Network/Client/Packet/Manager.hpp"
 #include "Parser/Parser.hpp"
 
 namespace raytracer::app
@@ -17,9 +20,16 @@ namespace raytracer::app
         void run() override;
         void stop() override;
 
+        void setRenderer(const yml::Yml &config) { this->_renderer = Renderer::fromConfig(config); }
+
+        void render(const uint32_t x, const uint32_t y, const uint32_t w, const uint32_t h) const
+            { this->_renderer.render(x, y, w, h); }
+
     private:
         network::Client _client;
-        bool _running{false};
+        network::packet::client::Manager _manager;
+        std::atomic_bool _running{false};
+        Renderer _renderer;
         // ...
     };
 
